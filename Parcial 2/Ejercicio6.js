fd993f5dc9f7693f7f4bbbbeb2d72fc107b85b2c
hm157 <a2221040018@alumnos.uat.edu.mx>
2024-11-06 15:40:02 -0600

    Add files via upload

---
+++ Parcial 2/Ejercicio6.js
@@ -0,0 +1,25 @@
+const rutaImagen='https://image.tmdb.org/t/p/w1280';
+const apiConsulta='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
+const contenedor= document.getElementById('contenedor');
+ 
+
+CargarPelicula();
+
+async function CargarPelicula()
+{
+   let consulta=await fetch (apiConsulta);
+   let resultado=await consulta.json();
+   let aleatorio= Math.floor(Math.random()*20)+1;
+   let pelicula= resultado.results[aleatorio];
+   console.log(pelicula);
+   let div= document.createElement('div');
+   let template='';
+   if (pelicula==null)
+   {
+      template=`<img src='${rutaImagen}' alt='sin imagen'/>`;
+   }
+   else {
+      template=`<img src='${rutaImagen+pelicula.poster_path}'/>`;
+   }
+}
+
